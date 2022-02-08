d2d9dd3ebc560d303010fc8f37435344686f497e
mrvalraven <akatiago@gmail.com>
2022-02-08 20:58:45 +0000

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
+
+dotenvLoad();
+
+const withEnv = nextEnv();
+
 module.exports = {
   reactStrictMode: true,
-}
+};
